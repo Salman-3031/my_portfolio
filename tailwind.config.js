d4d7ccfe04bee2cfafd115f7756c38ddd1cbacca
Salman-3031 <salman5223031@gmail.com>
2025-07-30 12:10:13 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     "./public/**/*.html",
-    "./public/javascript/**/*.js",
+    "./public/javaScript/**/*.js",
      "./public/css/**/*.css",
-     './public/img/**/*.{jpg,jpeg,png,webp}',
+     './public/img/**/*.{jpg,jpeg,png,webp,svg}',
      "./src/input.css"
   ],
  
